f8f0cb95f5a0168b9d8e9f1714c85037489672c9
janvi <janvi0217@gmail.com>
2022-04-29 12:59:09 +0530

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,14 +1,15 @@
-import React from "react";
+// import React from "react";
 
 //import component of pages
 import AboutSection from "../components/AboutSection";
 import Services from "../components/Services";
-
+import FaqSection from "../components/FaqSection";
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
       <Services />
+      <FaqSection />
     </div>
   );
 };
