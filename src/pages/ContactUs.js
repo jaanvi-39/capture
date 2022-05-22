4ccc54c04ac4d03146e67494a610a962cc126841
jaanvi-39 <gjaanvi39@gmail.com>
2022-05-22 23:37:36 +0530

    Contact Us

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,22 +1,70 @@
 import { motion } from "framer-motion";
-import { pageTransition } from "../animation";
+import styled from "styled-components";
+import { pageTransition, titleAnimation } from "../animation";
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       variants={pageTransition}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <p> Contact Us</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sunt
-        labore nesciunt mollitia? Nihil saepe delectus tempore quo eligendi
-        quibusdam eveniet sequi repudiandae aperiam illo obcaecati, inventore
-        rerum? Harum.
-      </p>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}> Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h3>Send us a message.</h3>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h3>Send an email.</h3>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h3>Social Media.</h3>
+        </Social>
+      </Hide>
+    </ContactStyle>
   );
 };
+const Circle = styled.div`
+  border-radius: 50%;
+  height: 2.5rem;
+  width: 2.5rem;
+  background: #353535;
+`;
+const Social = styled(motion.div)`
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  h3 {
+    margin: 2rem;
+    color: #353535;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  background: white;
+  color: #353535;
+  min-height: 90vh;
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+  h2 {
+    color: black;
+  }
+`;
 export default ContactUs;
